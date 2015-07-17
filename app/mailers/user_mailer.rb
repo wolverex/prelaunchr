9c2a142526005d92d2d03e8839eca7e88d70ff11
root <root@linjer.uneekly.com>
2015-07-17 00:30:25 -0400

    Email sender

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ActionMailer::Base
-    default from: "Harry's <welcome@harrys.com>"
+    default from: "Nicholas <nicholas@uneekly.com>"
 
     def signup_email(user)
         @user = user
